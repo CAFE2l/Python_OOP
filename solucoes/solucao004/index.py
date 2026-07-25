4b70da4de1a5c6444da0f5d10d28218d6a9a1bed
CAFE2l <pciskolargx@gmail.com>
2026-07-25 07:52:27 -0300

    test

--- solucoes/solucao004/index.py
+++ solucoes/solucao004/index.py
@@ -17,12 +17,27 @@ estilos = {
 }
 
 class Funcionario:
+    #Atributos de classe
+    empresa = "Curso em video"
+
+
     def __init__(self, nome, setor, cargo):
+        #Atributos de instancia
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
+    
+    def apresentacao(self) -> str:
+        return f":handshake: Hello I'm [blue][bold]{self.nome}[/bold][/blue] and work as [red][bold]{self.cargo}[/bold][/red] at the stage [green][bold]{self.setor}[/green][/bold] on the company [yellow][bold]{self.__class__.empresa}[/bold][/yellow]"
+
+
 
 
 
 c1 = Funcionario(nome="Maria", setor="Admin", cargo="diretora")
-inspect(c1)
+c1.empresa = "FIAP"
+
+c2 = Funcionario(nome="Pedro", setor="TI", cargo="Programador")
+print(c1.apresentacao())
+print(c2.apresentacao())
+
