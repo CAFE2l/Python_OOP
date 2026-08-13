69db5606309aeaf0856cd4652e3021b23b48b0fa
CAFE2l <pciskolargx@gmail.com>
2026-08-13 14:14:31 -0300

    DE CABECA FIO TOMA

--- Desafios/Desafio012/index.py
+++ Desafios/Desafio012/index.py
@@ -2,6 +2,7 @@ from rich import print
 from abc import ABC, abstractmethod
 import random
 
+
 class Personagem(ABC):
     def __init__(self, nome, vida):
         self.nome = nome
@@ -10,9 +11,8 @@ class Personagem(ABC):
 
     def atacar(self, alvo, forca):
         golpe = random.choice(self.golpes)
-        self.alvo = alvo
-        self.forca = forca
-        print(f"[bold][red]{self.nome} [/]atacou [blue]{alvo.nome}[/] com [yellow]{golpe}[/] de forca [green]{forca}[/]") 
+        print(f"{self.nome} Atacou {alvo.nome} com forca de {forca}")
+
         dano = int(forca * random.uniform(0.15, 0.95))
         alvo.receber_dano(dano)
 
@@ -20,41 +20,31 @@ class Personagem(ABC):
     def receber_dano(self, dano):
         self.vida -= dano
         if self.vida <= 0:
-            self.vida = 0
-
-        print(f"[bold][red]{self.nome}[/] recebeu dano de[green] {dano}[/]")
-
-    @abstractmethod
-    def curar(self):
-        pass
-
+            print(f"[bold][italic][red]ESTA NO CEMITERIO[/][/][/]")
+        else:
+            print(f"{self.nome} recebeu dano de {dano}")
+    
 
 class Guerreiro(Personagem):
     def __init__(self, nome, vida):
         super().__init__(nome, vida)
-        self.golpes = ["SOCAO", "VOADORA", "CHUTE NO SACU"]
+        self.golpes = ["SOCAO", "VOADORA", "CHUTE NO SACU", "FORA ZOIO", "PUXA TETA"]
 
     def curar(self):
-        cura = random.randint(100, 3000)
+        cura = random.randint(1000, 5000)
         self.vida += cura
-        print(f"{self.nome} usou pocao de cura e regenerou {cura} de vida")
+        print(f"{self.nome} usou pocao e regenerou {cura} de vida")
+
 
 class Mago(Personagem):
-    def __init__(self,nome, vida):
+    def __init__(self, nome, vida):
         super().__init__(nome, vida)
-        self.golpes = ["MACUMBA SARAVA", "SOLTA PEIDO E SAI FUBA", "PLIM PLIM"]
-
+        self.golpes = ["MACUMBA SARAVA", "MAGIA PRETA", "BRUXARIA"]
+        
     def curar(self):
-        cura = random.randint(100, 3000)
+        cura = random.randint(1000, 5000)
         self.vida += cura
-        print(f"[bold][cyan]{self.nome}[/] usou [red]macumba[/] e regenerou [green]{cura}[/] de vida[/]")
+        print(f"{self.nome} FEZ FEITICARIA e regenrou {cura} de vida")
 
+    
 
-
-
-g1 = Guerreiro("Kratos", 30000)
-m1 = Mago("Bruxa do 71", 71000)
-
-m1.atacar(g1, 10000)
-g1.atacar(m1, 20000)
-m1.curar()
