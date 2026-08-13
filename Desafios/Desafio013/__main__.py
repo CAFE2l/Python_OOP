7decb884ef1c71173f1245b645865fed227fe5de
CAFE2l <pciskolargx@gmail.com>
2026-08-13 16:36:03 -0300

    receba

--- Desafios/Desafio013/__main__.py
+++ Desafios/Desafio013/__main__.py
@@ -2,21 +2,15 @@ from index import *
 
 from rich import print, inspect
 
-
 def main():
-    t = Termostato(25)
+    t = Termostato()
+    try:
+        t.temperatura = 11.22
+        print(t.ftemperatura)
+    except Exception as e:
+        print(f"Houve um problema: {e}")
 
-    inspect(t, private=True, methods=True)
-
-    print(f"A temperatura atual é {t.ftemperatura}")
-
-    t.aumentar()
-    print(f"Depois de aumentar: {t.ftemperatura}")
-
-    t.diminuir()
-    t.diminuir()
-    print(f"Depois de diminuir duas vezes: {t.ftemperatura}")
-
+    print(f"A temperatura atual e de {t.ftemperatura}{chr(176)}C ")
 
 if __name__ == "__main__":
     main()
--- Desafios/Desafio013/index.py
+++ Desafios/Desafio013/index.py
@@ -1,44 +1,26 @@
 from rich import print, inspect
 
 class Termostato: 
-    MIN_TEMP = 16
-    MAX_TEMP = 30 
-    INCREMENTO = 0.5
+    def __init__(self):
+        self.__temperatura = 21
 
-    def __init__(self, temperatura=25):
-        self.__temperatura = None
-        self.temperatura = temperatura
-
-    
     @property
     def temperatura(self):
         return self.__temperatura
 
+
     @temperatura.setter
     def temperatura(self, valor):
-        if not (self.MIN_TEMP <= valor <= self.MAX_TEMP):
-            raise ValueError(f"A temperatura deve estar entre {self.MIN_TEMP} e {self.MAX_TEMP}")
-    
-        valor_ajustado = round(valor/self.INCREMENTO) * self.INCREMENTO
-        if valor_ajustado == int(valor_ajustado):
-            valor_ajustado = int(valor_ajustado)
-        self.__temperatura = valor_ajustado
+        if valor % 0.5 != 0:
+            raise ValueError(f"Temperatura de {valor}{chr(176)}C its invalid")
+        if valor < 16:
+            self.__temperatura = 16
+        elif valor > 30:
+            self.__temperatura = 30 
+        else:
+            self.__temperatura = valor 
 
-    def aumentar(self):
-        nova_temp = self.temperatura + self.INCREMENTO
-        if nova_temp > self.MAX_TEMP:
-            print("Temperatura maxima atingida")
-        return 
-        self.temperatura = nova_temp
-
-
-    def diminuir(self):
-        nova_temp = self.temperatura - self.INCREMENTO
-        if nova_temp < self.MIN_TEMP:
-            print("Temperatura minima atingida")
-        return  
-        self.temperatura = nova_temp
-
+    @property
     def ftemperatura(self):
-        return f"{self.temperatura}C"
+        return f"{self.__temperatura}{chr(176)}C"
 
